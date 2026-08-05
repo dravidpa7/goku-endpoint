7c06cb65ddb6dd7f02a191434534f286804d35c6
dravidpa7 <dravidpa20@gmail.com>
2026-08-05 23:58:30 +0530

    feat: refactor SVG generation and add static file serving

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ import express, { json } from "express";
 import fetchContributions from "./lib/github.js";
 import {buildSvg} from "./lib/svg.js";
 import { LongestStreak } from "./lib/logic.js";
+import path from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.static(path.join(process.cwd(),'public')));
 
 app.get("/api/contributions", async (req,res) => {
     const username = req.query.username;
@@ -34,7 +36,10 @@ app.get("/graph", async (req,res) => {
     try {
         const data = await fetchContributions(username);
         const streak = LongestStreak(data);
-        const svg = buildSvg(data.days);
+
+        console.log(streak);
+        
+        const svg = buildSvg(streak);
         res.setHeader("Content-Type", "image/svg+xml");
         res.setHeader("Cache-Control", "public, max-age=3600");
         res.send(svg);
